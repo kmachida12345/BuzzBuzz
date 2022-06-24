6fd172971782f0828d8e29874bfa846c56883aa8
kohei.machida <kohei.machida@a10lab.com>
2022-06-24 13:02:39 +0900

    fix layouts

--- app/src/main/java/com/github/kmachida12345/myapplication/MainActivity.kt
+++ app/src/main/java/com/github/kmachida12345/myapplication/MainActivity.kt
@@ -3,9 +3,7 @@ package com.github.kmachida12345.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardOptions
@@ -15,6 +13,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
@@ -56,15 +55,18 @@ fun Greeting(name: String) {
         }
     ) {
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize().padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             LazyColumn(
-                modifier = Modifier.weight(1f),
+                modifier = Modifier.weight(1f).fillMaxWidth(),
                 verticalArrangement = Arrangement.spacedBy(4.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
                 itemsIndexed(patternElements) { index, item ->
-                    Card {
+                    Card(
+                    ) {
                         TextField(
                             value = patternElements[index].toString(),
                             onValueChange = {
